2148aadf11201d5b70766580151e134f6cab0f65
zahrasalarian <zasalarian2000@gmail.com>
2021-01-31 12:36:56 +0330

    Added some funs for getParentID

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -532,3 +532,8 @@ procdump(void)
     cprintf("\n");
   }
 }
+
+int getparentid()
+{
+  return 12;
+}
---
+++ xv6-public/testprogram2_1.c
@@ -0,0 +1,8 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(){
+    printf(1, "code is: %d\n", getparentid());
+    return 0;
+}
