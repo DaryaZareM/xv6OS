e6a1c3e08444fdb3827dab3f4e0ba99b1eb1b9dc
zahrasalarian <zasalarian2000@gmail.com>
2021-02-02 15:01:05 +0330

    Added QUANTUM to personalize the Round Robin algorithm

--- xv6-public/trap.c
+++ xv6-public/trap.c
@@ -103,7 +103,7 @@ trap(struct trapframe *tf)
   // Force process to give up CPU on clock tick.
   // If interrupts were on while locks held, would need to check nlock.
   if(myproc() && myproc()->state == RUNNING &&
-     tf->trapno == T_IRQ0+IRQ_TIMER)
+     tf->trapno == T_IRQ0+IRQ_TIMER && ticks == QUANTUM)
     yield();
 
   // Check if the process has been killed since we yielded
